00b4a91ac6b232477e00af18f55c9ca15cbe9aad
Mohak Patel <patelbmohak@gmail.com>
2018-01-29 23:30:08 -0500

    :heavy_plus_sign: Comment ptsIn_x*subset

--- ptsIn_x0subset.m
+++ ptsIn_x0subset.m
@@ -1,6 +1,20 @@
 function [idx] = ptsIn_x0subset(x,sPt,sSize,tIdx)
-% Find points in subset in x0;
-%x0ss
+%
+% [[idx] = ptsIn_x0subset(x,sPt,sSize,tIdx) Finds untracked particles in
+% the subset of the reference image
+%
+% INPUTS
+% -------------------------------------------------------------------------
+%   x:          List of paritlces in reference image (nx3)
+%   sPT:        Subset starting point (1x3)
+%   sSize:      Subset size (1x3)
+%   tIdx:       Index of tracked particles in x
+%
+%  OUTPUTS
+%  ------------------------------------------------------------------------
+%   idx:        Index of untracked particles in the given subset
+%
+
 
 % Define range of points within subset
 minPt = sPt;  %min
@@ -11,9 +25,11 @@ idx = zeros(size(x));
 for i = 1:3
      idx(:,i) = x(:,i) >= minPt(i) & x(:,i) < maxPt(i);
 end
+
 idx = logical(prod(idx,2));
 idx = find(idx);
-idx = idx.*(~tIdx(idx));
+idx = idx.*(~tIdx(idx)); %save only untracked index
 idx = nonzeros(idx);
+
 end
 
